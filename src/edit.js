53a7f55638a20fc740205da9fc603dd01495be3d
Marko Krstic <mailme@markokrstic.com>
2023-05-04 17:17:05 +0200

    ..

--- src/edit.js
+++ src/edit.js
@@ -1,45 +1,71 @@
-import { 
-  Panel, 
-  PanelBody, 
-  PanelRow, 
-  ColorPalette, 
-  RangeControl, 
-  ToggleControl 
-} from '@wordpress/components';
-
 import { InspectorControls } from '@wordpress/block-editor';
-
 import { __ } from '@wordpress/i18n';
 import { useBlockProps } from '@wordpress/block-editor';
 import { ServerSideRender } from '@wordpress/editor';
 import './editor.scss';
 
+import {
+  ColorPalette,
+  RangeControl,
+  ToggleControl,
+  TabPanel,
+} from '@wordpress/components';
+
 const Edit = (props) => {
-
-  const wrapperPanel = (
-    <Panel header="Task Manager Settings">
-      <PanelBody title="Wrapper" initialOpen={ false }>
-          <PanelRow>Wrapper Settings - My Panel Inputs and Labels</PanelRow>
-      </PanelBody>
-      <PanelBody title="Columns" initialOpen={ false }>
-          <PanelRow>Columns Settings - My Panel Inputs and Labels</PanelRow>
-      </PanelBody>
-      <PanelBody title="Cards" initialOpen={ false }>
-          <PanelRow>Cards Settings - My Panel Inputs and Labels</PanelRow>
-      </PanelBody>
-    </Panel>    
-  );
-
   return (
-    <div { ...useBlockProps() }>
+    <div {...useBlockProps()}>
       <ServerSideRender
         block="tasks-manager/tasks-progress"
         attributes={props.attributes}
       />
       <InspectorControls>
-        { wrapperPanel }
+        <TabPanel
+          className="my-tabs"
+          activeClass="is-active"
+          tabs={[
+            {
+              name: 'wrapper',
+              title: __('Wrapper'),
+              className: 'my-tab-wrapper',
+            },
+            {
+              name: 'columns',
+              title: __('Columns'),
+              className: 'my-tab-columns',
+            },
+            {
+              name: 'cards',
+              title: __('Cards'),
+              className: 'my-tab-cards',
+            },
+          ]}
+        >
+          {(tab) => {
+            switch (tab.name) {
+              case 'wrapper':
+                return (
+                  <div className="my-tab-panel">
+                    <ToggleControl label={ __('My Toggle Control') } />
+                  </div>
+                );
+              case 'columns':
+                return (
+                  <div className="my-tab-panel">
+                    <ColorPalette label={ __('My Color Palette') } />
+                  </div>
+                );
+              case 'cards':
+                return (
+                  <div className="my-tab-panel">
+                    <ToggleControl label={ __('My Other Toggle Control') } />
+                  </div>
+                );
+              default:
+                return null;
+            }
+          }}
+        </TabPanel>
       </InspectorControls>
-
     </div>
   );
 };

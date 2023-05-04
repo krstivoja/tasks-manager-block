5351635b1c29f7e228b3572530a96a5372a3a347
Marko Krstic <mailme@markokrstic.com>
2023-05-04 14:10:55 +0200

    added panels

--- src/edit.js
+++ src/edit.js
@@ -1,15 +1,37 @@
+import { Panel, PanelBody, PanelRow } from '@wordpress/components';
+import { InspectorControls } from '@wordpress/block-editor';
+
 import { __ } from '@wordpress/i18n';
 import { useBlockProps } from '@wordpress/block-editor';
 import { ServerSideRender } from '@wordpress/editor';
 import './editor.scss';
 
 const Edit = (props) => {
+
+  const wrapperPanel = (
+    <Panel header="Task Manager Settings">
+      <PanelBody title="Wrapper" initialOpen={ false }>
+          <PanelRow>Wrapper Settings - My Panel Inputs and Labels</PanelRow>
+      </PanelBody>
+      <PanelBody title="Columns" initialOpen={ false }>
+          <PanelRow>Columns Settings - My Panel Inputs and Labels</PanelRow>
+      </PanelBody>
+      <PanelBody title="Cards" initialOpen={ false }>
+          <PanelRow>Cards Settings - My Panel Inputs and Labels</PanelRow>
+      </PanelBody>
+    </Panel>    
+  );
+
   return (
     <div { ...useBlockProps() }>
       <ServerSideRender
         block="tasks-manager/tasks-progress"
         attributes={props.attributes}
       />
+      <InspectorControls>
+        { wrapperPanel }
+      </InspectorControls>
+
     </div>
   );
 };

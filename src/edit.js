8ddaa7b840383938e8702040ff5a631a828a6f9e
Marko Krstic <mailme@markokrstic.com>
2023-05-05 12:50:03 +0200

    added color palete

--- src/edit.js
+++ src/edit.js
@@ -1,30 +1,28 @@
 import { __ } from '@wordpress/i18n';
-import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
-import { PanelBody, ColorPicker } from '@wordpress/components';
+import { useBlockProps, InspectorControls, useSetting } from '@wordpress/block-editor';
+import { PanelBody, ColorPicker, ColorPalette } from '@wordpress/components';
 import ServerSideRender from '@wordpress/server-side-render';
 
 const Edit = (props) => {
     const { attributes, setAttributes } = props;
     const { backgroundColor } = attributes;
 
-    const updateBackgroundColor = (value) => {
-        setAttributes({ backgroundColor: value.hex });
+    const setBackgroundColor = (value) => {
+        setAttributes({ backgroundColor: value });
     };
 
     return (
         <div {...useBlockProps()}>
             <InspectorControls>
-                <PanelBody title={__('Background Color', 'tasks-manager')}>
-                    <ColorPicker
-                        color={backgroundColor}
-                        onChangeComplete={updateBackgroundColor}
+                <PanelBody title={__('Background Color', 'tasks-manager')} >
+                    <ColorPalette
+                        value={backgroundColor}
+                        colors={[...useSetting('color.palette')]}
+                        onChange={setBackgroundColor}
                     />
                 </PanelBody>
             </InspectorControls>
-            <ServerSideRender
-                block="tasks-manager/tasks-progress"
-                attributes={attributes}
-            />
+            <ServerSideRender block="tasks-manager/tasks-progress" attributes={attributes} />
         </div>
     );
 };

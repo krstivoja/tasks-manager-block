3978486779a0c4ede0ab909b0136f80672c5d716
Marko Krstic <mailme@markokrstic.com>
2023-05-14 12:38:27 +0200

    Updating Block settings

--- src/components/GapControl.js
+++ src/components/GapControl.js
@@ -1,34 +1,57 @@
-import { RangeControl, Button, __experimentalUnitControl as UnitControl } from '@wordpress/components';
+import {  RangeControl, 
+  Button, 
+  __experimentalUnitControl as UnitControl, 
+  __experimentalBoxControl as BoxControl,
+  Icon, 
+  Flex, 
+  FlexItem,
+  BaseControl } from '@wordpress/components';
+
 import { useSetting } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
+import { link, adminGeneric } from '@wordpress/icons';
+import { useState } from '@wordpress/element'; // import useState
 
 const GapControl = ({ value, onChange }) => {
-  const spacingSizes = [...useSetting('spacing.spacingSizes')];
-  const additionalSizes = [{ value: 0 }];
-  const marks = additionalSizes.concat(spacingSizes.map(s => ({ value: s.slug })));
-  const max = spacingSizes.length;
+const spacingSizes = [...useSetting('spacing.spacingSizes')];
+const additionalSizes = [{ value: 0 }];
+const marks = additionalSizes.concat(spacingSizes.map(s => ({ value: s.slug })));
+const max = spacingSizes.length;
 
-  return (
-    <>
-      <Button variant="secondary" label={__('Set custom size', 'tasks-manager')} className="gap-button" icon="admin-settings" onClick={() => {
-        const el = document.querySelector('.gap-range-control');
-        if (el) {
-          el.classList.toggle('visible');
-        }
-      }}></Button>
-      <UnitControl onChange={onChange} value={value} label={__('Block spacing', 'tasks-manager')} className="gap-text" />
-      <RangeControl
-        label={__('Block spacing', 'tasks-manager')}
-        initialPosition={marks.findIndex(p => p.value == parseInt(value, 10))}
-        className="gap-range-control"
-        separatorType="topFullWidth"
-        withInputField="false"
-        max={max}
-        value={value}
-        onChange={(newValue) => { onChange(`${marks[newValue]["value"]}px`); }}
-      />
-    </>
-  );
+const [showUnitControl, setShowUnitControl] = useState(true); // state for toggling
+
+return (
+<>
+<Flex direction={['column', 'row']}>
+<label>Block spacings</label>
+<Button 
+  variant="secondary"  className="components-button is-small has-icon"
+  onClick={() => setShowUnitControl(!showUnitControl)}>
+  <Icon icon={ link } />
+</Button>
+</Flex>
+<h2>Marko</h2>
+
+{showUnitControl ? (
+<UnitControl onChange={onChange} value={value} className="" />
+) : (
+<RangeControl
+  initialPosition={marks.findIndex(p => p.value == parseInt(value, 10))}
+  className=""
+  separatorType="topFullWidth"
+  withInputField={false}
+  max={max}
+  value={value}
+  onChange={(newValue) => { onChange(`${marks[newValue]["value"]}px`); }}
+/>
+)}
+
+<BoxControl allowReset="false" className="" label="PADDING" value={value} onChange={onChange} />
+
+<h2>Marko</h2>
+
+</>
+);
 };
 
 export default GapControl;

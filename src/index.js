3fa325eea138f1c8acbfe5dbec0294a6eafb1e5d
Zafar Kamal <sk4915497@gmail.com>
2023-05-15 23:21:32 +0500

    :sparkles: Refactor, fixes and Added Caching

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import { __ } from '@wordpress/i18n';
-import { registerBlockType } from '@wordpress/blocks';
-import Edit from './edit';
-import Save from './save';
-import TaskList from './tasksList.js';
-import './style.scss';
+import { __ } from "@wordpress/i18n";
+import { registerBlockType } from "@wordpress/blocks";
+import Edit from "./edit";
+import "./style.scss";
 
-registerBlockType('tasks-manager/tasks-progress', {
-  title: __('Tasks Manager Block'),
-  icon: 'calendar',
-  category: 'common',
+registerBlockType("tasks-manager/tasks-progress", {
+  title: __("Tasks Manager Block"),
+  icon: "calendar",
+  category: "common",
   edit: Edit,
-  save: Save
+
+  // Handled via PHP (Server Side).
+  save: () => null,
 });
---
+++ src/tasks-list.js
@@ -0,0 +1,129 @@
+/**
+ * WordPress Dependencies
+ */
+import { isEmpty } from "lodash";
+import { useSelect } from "@wordpress/data";
+import { Placeholder, Spinner } from "@wordpress/components";
+
+function TaskList(props) {
+  const { terms, hasTermsResolved } = useSelect((select) => {
+    const selectorArgs = [
+      "taxonomy",
+      "progress",
+      {
+        per_page: -1, // So that it pulls in every term.
+      },
+    ];
+
+    return {
+      /**
+       * Using getEntityRecords so that the result is cached accross the block.
+       */
+      terms: select("core").getEntityRecords(...selectorArgs),
+      hasTermsResolved: select("core").hasFinishedResolution(
+        "getEntityRecords",
+        selectorArgs
+      ),
+    };
+  }, []);
+
+  const { tasks, hasTasksResolved } = useSelect((select) => {
+    const selectorArgs = [
+      "postType",
+      "tasks",
+      {
+        per_page: 100,
+      },
+    ];
+
+    return {
+      tasks: select("core").getEntityRecords(...selectorArgs),
+      hasTasksResolved: select("core").hasFinishedResolution(
+        "getEntityRecords",
+        selectorArgs
+      ),
+    };
+  }, []);
+
+  if (!hasTermsResolved || !hasTasksResolved) {
+    return (
+      <Placeholder>
+        <Spinner />
+      </Placeholder>
+    );
+  }
+
+  const getGroupedTasks = () => {
+    // Safely exiting in case of error.
+    if (isEmpty(tasks) || isEmpty(terms)) {
+      return {};
+    }
+
+    return tasks.reduce((groups, task) => {
+      const progressTermId = task.progress[0];
+      if (!groups[progressTermId]) {
+        groups[progressTermId] = [];
+      }
+      groups[progressTermId].push(task);
+      return groups;
+    }, {});
+  };
+
+  const groupedTasks = getGroupedTasks();
+
+  return (
+    <div
+      className="tasks-shortcode"
+      style={{
+        backgroundColor: props.backgroundColor,
+        gap: props.gapSize,
+        paddingTop: props.paddingSize["top"],
+        paddingLeft: props.paddingSize["left"],
+        paddingRight: props.paddingSize["right"],
+        paddingBottom: props.paddingSize["bottom"],
+      }}
+    >
+      {terms.map((term) => (
+        <div key={term.id} className="tasks-group-wrap">
+          <h3>{term.name}</h3>
+          {groupedTasks[term.id] && groupedTasks[term.id].length > 0 ? (
+            <div
+              className="tasks-list"
+              style={{
+                backgroundColor: props.tasksListBackgroundColor,
+                gap: props.sgapSize,
+                paddingTop: props.spaddingSize["top"],
+                paddingLeft: props.spaddingSize["left"],
+                paddingRight: props.spaddingSize["right"],
+                paddingBottom: props.spaddingSize["bottom"],
+              }}
+            >
+              {groupedTasks[term.id].map((task) => {
+                return (
+                  <div
+                    key={task.id}
+                    className="task-item"
+                    style={{ backgroundColor: props.cardBackgroundColor }}
+                  >
+                    <span className="task-title">{task.title.rendered}</span>
+
+                    <button
+                      className="read-more wp-element-button"
+                      data-id={task.id}
+                    >
+                      Read More
+                    </button>
+                  </div>
+                );
+              })}
+            </div>
+          ) : (
+            <p>No tasks found.</p>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default TaskList;

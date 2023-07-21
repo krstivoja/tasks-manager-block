cd739c81c5132c4688f7a52d8f213aa405866250
Marko Krstic <mailme@markokrstic.com>
2023-07-21 19:15:36 +0200

    I added the "tm_" prefix to the function names to make them unique to your plugin

--- inc/register.php
+++ inc/register.php
@@ -1,69 +1,74 @@
 <?php
 
+// Prevent Direct File Access
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+
 // Register custom post type "tasks"
-add_action('init', 'register_tasks_post_type');
-function register_tasks_post_type() {
-  $labels = array(
-    'name' => __('Tasks'),
-    'singular_name' => __('Task'),
-    'add_new' => __('Add New'),
-    'add_new_item' => __('Add New Task'),
-    'edit_item' => __('Edit Task'),
-    'new_item' => __('New Task'),
-    'view_item' => __('View Task'),
-    'search_items' => __('Search Tasks'),
-    'not_found' => __('No tasks found'),
-    'not_found_in_trash' => __('No tasks found in Trash'),
-    'parent_item_colon' => '',
-    'menu_name' => __('Tasks')
-  );
+add_action('init', 'tm_register_tasks_post_type');
 
-  $args = array(
-    'labels' => $labels,
-    'public' => true,
-    'has_archive' => false,
-    'menu_icon' => 'dashicons-performance',
-    'supports' => array('title', 'editor'),
-    'show_ui' => true,
-    'capability_type' => 'post',
-    'rewrite' => array('slug' => 'tasks'),
-    'menu_position' => 5,
-    'show_in_rest' => true, // Enable support for the block editor
-  );
+function tm_register_tasks_post_type()
+{
+    $labels = array(
+        'name' => __('Tasks'),
+        'singular_name' => __('Task'),
+        'add_new' => __('Add New'),
+        'add_new_item' => __('Add New Task'),
+        'edit_item' => __('Edit Task'),
+        'new_item' => __('New Task'),
+        'view_item' => __('View Task'),
+        'search_items' => __('Search Tasks'),
+        'not_found' => __('No tasks found'),
+        'not_found_in_trash' => __('No tasks found in Trash'),
+        'parent_item_colon' => '',
+        'menu_name' => __('Tasks')
+    );
 
-  register_post_type('tasks', $args);
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'has_archive' => false,
+        'menu_icon' => 'dashicons-performance',
+        'supports' => array('title', 'editor'),
+        'show_ui' => true,
+        'capability_type' => 'post',
+        'rewrite' => array('slug' => 'tasks'),
+        'menu_position' => 5,
+        'show_in_rest' => true, // Enable support for the block editor
+    );
+
+    register_post_type('tasks', $args);
 }
 
 // Register custom taxonomy "progress"
-add_action('init', 'register_progress_taxonomy');
-function register_progress_taxonomy() {
-  $labels = array(
-    'name' => __('Progress'),
-    'singular_name' => __('Progress'),
-    'search_items' => __('Search Progresses'),
-    'all_items' => __('All Progresses'),
-    'parent_item' => __('Parent Progress'),
-    'parent_item_colon' => __('Parent Progress:'),
-    'edit_item' => __('Edit Progress'),
-    'update_item' => __('Update Progress'),
-    'add_new_item' => __('Add New Progress'),
-    'new_item_name' => __('New Progress Name'),
-    'menu_name' => __('Progress')
-  );
+add_action('init', 'tm_register_progress_taxonomy');
 
-  $args = array(
-    'hierarchical' => true,
-    'labels' => $labels,
-    'show_ui' => true,
-    'show_admin_column' => true,
-    'query_var' => true,
-    'rewrite' => array('slug' => 'progress'),
-    'show_in_rest' => true, // Enable support for the block editor
-  );
+function tm_register_progress_taxonomy()
+{
+    $labels = array(
+        'name' => __('Progress'),
+        'singular_name' => __('Progress'),
+        'search_items' => __('Search Progresses'),
+        'all_items' => __('All Progresses'),
+        'parent_item' => __('Parent Progress'),
+        'parent_item_colon' => __('Parent Progress:'),
+        'edit_item' => __('Edit Progress'),
+        'update_item' => __('Update Progress'),
+        'add_new_item' => __('Add New Progress'),
+        'new_item_name' => __('New Progress Name'),
+        'menu_name' => __('Progress')
+    );
 
-  register_taxonomy('progress', array('tasks'), $args);
+    $args = array(
+        'hierarchical' => true,
+        'labels' => $labels,
+        'show_ui' => true,
+        'show_admin_column' => true,
+        'query_var' => true,
+        'rewrite' => array('slug' => 'progress'),
+        'show_in_rest' => true, // Enable support for the block editor
+    );
+
+    register_taxonomy('progress', array('tasks'), $args);
 }
-
-
-
-?>
